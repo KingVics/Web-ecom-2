fc90bb3939e28ae3c6b70c86aed24b1c77f9d01c
Okoromi Victor <okoromivictorsunday@gmail.com>
2020-10-17 17:07:11 +0100

    Login Form and Registration created

--- web-ecom/src/app/app.module.ts
+++ web-ecom/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component'
+import {RegistrationComponent} from './registration/register.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
